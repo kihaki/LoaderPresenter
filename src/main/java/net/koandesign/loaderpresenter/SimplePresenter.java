b74af5d6bbf0cc8e12c7fa3b480004bfa80da0a0
Alexander Steenbergen <kihaki@gmail.com>
2017-04-06 18:18:02 +0200

    Enable choice overriding of presenter methods

---
+++ src/main/java/net/koandesign/loaderpresenter/SimplePresenter.java
@@ -0,0 +1,22 @@
+package net.koandesign.loaderpresenter;
+
+
+import android.support.annotation.NonNull;
+
+public class SimplePresenter<V> extends Presenter<V> {
+
+	@Override
+	protected void onDestroyPresenter() {
+
+	}
+
+	@Override
+	protected void onAttachView(@NonNull V view) {
+
+	}
+
+	@Override
+	protected void onDetachView() {
+
+	}
+}
